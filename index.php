0ea4279ef5e2e62205958e1098b837e12f430af3
Mustafa Mohamed <96398424+Muztafa74@users.noreply.github.com>
2022-09-23 17:22:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Job Builder</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+   
+
+
+</head>
+<body>
+
+<header class="header fixed-top">
+
+   <div class="container">
+
+      <div class="row align-items-center justify-content-between">
+
+         <a href="#home" class="logo">Job  <span>  builder </span></a>
+
+         <nav class="nav">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#LatestJobs">Latest Jobs</a>
+            <a href="#reviews">reviews</a>
+            <a href="#contact">contact</a>
+         </nav>
+
+         <a href="#contact" class="link-btn">Apply for a job</a>
+         <nav class = "nav" ><a href="./log in/log_in.php">Log in</a></nav>
+         <a href="./sing up/sing_up.php" class="link-btn" >sing up</a>
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </div>
+
+   </div>
+
+</header>
+<section class="home" id="home">
+
+   <div class="container">
+
+      <div class="row min-vh-100 align-items-center">
+         <div class="content text-center text-md-left  ">
+            <h3>find Your Job</h3>
+            <p>Search by skills. View salaries. One-click apply.</p>
+            <a href="#contact" class="link-btn">Apply for a job</a>
+            </br></br>
+            <a href="./log in/log_in.php" class="link-btn">Log In</a>
+            </br></br>
+            <a href="./sing up/sing_up.php" class="link-btn">Sing Up</a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+<section class="about" id="about">
+
+   <div class="container">
+
+      <div class="row align-items-center">
+
+         <div class="col-md-6 image">
+            <img src="images/about-img.jpg" class="w-100 mb-5 mb-md-0" alt="">
+         </div>
+
+         <div class="col-md-6 content">
+            <span>about us</span>
+            <h3>Job Builder helps you in your job search in Egypt.</h3>
+            <p>Searching for jobs in Egypt? Job Builder helps you in your online job search to find Jobs in Egypt and Middle East. Choose the right job using our online recruitment services.</p>
+            <a href="#contact" class="link-btn">Apply for a job</a>
+         </div>
+
+      </div>
+
+   </div>
+
+<section class="LatestJobs" id="LatestJobs">
+
+   <h1 class="heading">Latest Jobs</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/icon1.png" alt="">
+         <h3>.Net developert</h3>
+         <P align ="left">Salary = 1000$   </p>
+         <p align ="left">Positions = 16   </p>
+         <h4  style="color:#00b8b8;">Job Requirements : </h4>
+         <p>at least 1 year of experience in a similar role
+         Strong knowledge of .Net, Asp.net & C sharp
+         Very good English
+         Technical skills: Communication Skills 
+         – Critical thinking Skills
+         – Analytical Skills
+         – Coding Skills</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon2.png" alt="">
+         <h3>Moderation</h3>
+         <p align ="left">Salary = 900$   </p>
+         <p align ="left">Positions = 7   </p>
+         <h4  style="color:#00b8b8;">Job Requirements : </h4>
+         <p>Proactive and energetic.
+            Very good Communication skills.
+            Ability to work independently and in a team environment.
+            Active Person
+            Experience with social media moderating for at least 1 year
+            Strong, professional written and verbal communication skills.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon3.png" alt="">
+         <h3>Senior UI/UX Designer</h3>
+         <p align ="left">Salary = 1200$   </p>
+         <p align ="left">Positions = 3   </p>
+         <h4  style="color:#00b8b8;">Job Requirements : </h4>
+         <p>5+ years of experience working as a Web UI/UX designer.
+            Must have excellent knowledge of all aspects of print, web and mobile apps designs.
+            Exceptional problem solving and ability to work independently
+            Exceptional learning skills 
+            Good communication in both oral and written English and Arabic</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon4.png" alt="">
+         <h3>Marketing Agent</h3>
+         <p align ="left">Salary = 850$   </p>
+         <p align ="left">Positions = 12   </p>   
+         <h4 style="color:#00b8b8;">Job Requirements : </h4>      
+            <p>Good English language
+            Good Knowledge in internet and technology
+            Good speaker and presentable
+            3+ years' experience in a marketing role
+            Ability to deal with different cultures.
+            Ability to learn about products and services and describe/explain them to prospects.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon5.png" alt="">
+         <h3>Senior PHP Backend Developer</h3>
+         <p align ="left">Salary = 1300$   </p>
+         <p align ="left">Positions = 6  </p>
+         <h4  style="color:#00b8b8;">Job Requirements : </h4>
+         <p>Must have:
+            Bachelor of Computer Science or Engineering or an equivalent.
+            3+ years’ experience in web development.
+            Excellent understanding of OOP and design patterns.
+            Expertise in developing web applications using PHP frameworks (Laravel )
+            Expertise in Javascript, JQuery, AngularJS, NodeJS, HTML and CSS.
+            Familiarity with GIT.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon6.png" alt="">
+         <h3>Software Tester (QC)</h3>
+         <p align ="left">Salary = 1100$   </p>
+         <p align ="left">Positions = 7   </p>
+         <h4  class ="sasa" style="color:#00b8b8;">Job Requirements : </h4>
+         <p>Previous working experience as a software developer is a plus
+            Strong inclination towards QA automation
+            Proven ability to work with teams and build a high-performance culture
+            Solid understanding of software development, debugging, testing, and problem-
+            solving.
+            </p>
+      </div>
+
+   </div>
+
+</section>
+<section class="process">
+
+   <h1 class="heading">Jobs by Career Level</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="./images/student.jpg" alt="">
+         <h3>Student</h3>
+         <p>HR Personnel Section HeadBusiness Development </br>
+            ERP Coordinator </br>
+            Administrative Assistant </br>
+            HR Coordinator - Internship </br> 
+            Mathematics Teacher Assistant </br> 
+            B2B Sales Representative
+            </br>
+            Editorial Intern </br>
+            Planning Internship </br> 
+            Production Engineering - Internship
+         
+         </p>
+      </div>
+
+      <div class="box">
+         <img src="./images/manegar.jpg" alt="">
+         <h3>Manager</h3>
+         <p>Java Spring Developer </br>
+         Sales Project Manager / English Speaking</br>
+         Sales Manager </br>
+         Production Manager</br>
+         Marketing Manager (Real estate) </br>
+         Yodawy Product Manager
+         </br>Maintenance Section Head </br>
+         Conference Manager - Tourism</br> 
+         Finance Manager</br>
+         Wholesale Sales Supervisor - FMCG </br>
+          
+      </div>
+
+      <div class="box">
+         <img src="./images/SeniorManagement.jpg" alt="">
+         <h3>Senior Management</h3>
+         <p>Senior Cost Accountant </br>
+         Procurement (Supervisor)</br>
+         HR Director (Real Estate - Multinational)</br>
+         Commercial Director (Large Real Estate)</br>
+         Sr. Supervisor - Projects</br>
+         HR Director</br>
+         Planning Engineer</br>
+         Projects Director</br>
+         Head of Marketing</br>
+         Planning & Cost Control Section Head</br>
+
+         </p>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+<section class="reviews" id="reviews">
+
+   <h1 class="heading"> Team Members </h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/eng-mohamed.png" alt="">
+         <p> </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Mohamed Mustafa</h3>
+         <span>Instructor</span>
+      </div>
+
+      <div class="box">
+         <img src="images/mustafa.jpg" alt="">
+         <p></p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Mustafa Mohamed</h3>
+         <span>Student</span>
+      </div>
+      <div class="box">
+         <img src="images/ahmed.jpg" alt="">
+         <p></p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Ahmed Arab</h3>
+         <span>Student</span>
+      </div>
+
+</section>
+
+<section class="contact" id="contact">
+
+   <h1 class="heading">Apply For A Job</h1>
+
+   <form method="post">
+
+      <span>your name :</span>
+      <input type="text" name="name" placeholder="enter your name" class="box" required>
+      <span>your email :</span>
+      <input type="email" name="email" placeholder="enter your email" class="box" required>
+      <span>your number :</span>
+      <input type="number" name="number" placeholder="enter your number" class="box" required>
+      <span>Job Name :</span>
+      <input type="text" name="name" placeholder="enter Job name" class="box" required>
+      
+      <span>Date of birth :</span>
+      <input type="date" name="birthday" class="box" required>
+      <input type="submit" value="Apply For A Job" name="submit" class="link-btn">
+   </form>  
+
+</section>
+<section class="footer">
+
+   <div class="box-container container">
+
+      <div class="box">
+         <i class="fas fa-phone"></i>
+         <h3>phone number</h3>
+         <p>01019358535</p>
+         <p>01223810298</p>
+      </div>
+      
+      <div class="box">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>our address</h3>
+         <p>KFS, Egypt</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-clock"></i>
+         <h3>opening hours</h3>
+         <p>00:07am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-envelope"></i>
+         <h3>email address</h3>
+         <p>muztafalap@gmail.com</p>
+
+      </div>
+
+   </div>
+<script src="js/script.js"></script> 
+
+
+
+</body>
+</html>
